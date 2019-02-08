5ee4ef60bce01b3e8e2135e253cef82262599a24
nnaemeka obioha <nnaemeka.obioha@gmail.com>
2019-02-08 06:36:35 +0100

    Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import MenuIcon from '@material-ui/core/Menu'
+import IconButton from '@material-ui/core/IconButton'
+
+
+export default function Header() {
+  return (
+    <AppBar position="fixed">
+
+    </AppBar>
+  )
+}
